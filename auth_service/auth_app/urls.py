c95c6e8ae02d5530347a01ca0c732b6cb826ac32
MaxiSnowPython <maximbobchik@gmail.com>
2026-01-09 21:06:37 +0100

    reg service working

--- auth_service/auth_app/urls.py
+++ auth_service/auth_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
-
+from .views import RegView,LogView
 urlpatterns = [
-
+    path('register/',RegView.as_view(), name="register"),
+    path('login/',LogView.as_view(),name="login"),
 ]
--- auth_service/auth_app/views.py
+++ auth_service/auth_app/views.py
@@ -4,10 +4,11 @@ from django.contrib.auth import login
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 # Create your views here.
 
 class RegView(FormView):
-    template_name = ""
+    template_name = "reg_form.html"
     form_class = UserCreationForm
     def form_valid(self, form):
         user = form.save()
@@ -15,7 +16,20 @@ class RegView(FormView):
         refresh = RefreshToken.for_user(user)
         refresh['username']=user.username
         access_token = str(refresh.access_token)
-
+        print("Zaregalsa")
+        return redirect(f"http://127.0.0.1:8001/match/hub/?token={access_token}")
+    def form_invalid(self, form):
+        return super.form_invalid(form)
+    
+class LogView(LoginView):
+    template_name = 'log_form.html'
+    def form_valid(self, form):
+        user = form.get_user()
+        login(self.request,user)
+        refresh = RefreshToken.for_user(user)
+        refresh['username']=user.username
+        access_token = str(refresh.access_token)
+        print("Zaloginilsa")
         return redirect(f"http://127.0.0.1:8001/match/hub/?token={access_token}")
     def form_invalid(self, form):
         return super.form_invalid(form)
